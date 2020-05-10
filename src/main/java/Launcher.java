48f6ee1898405fae6b7662516f42b707538ed79f
ClarFleu <clarisse.fleu@gmail.com>
2020-05-10 11:42:29 +0200

    Add a GameCreator classe

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,16 +1,32 @@
-import GameBoard.Board;
-import Player.Player;
+
+import java.io.*;
 
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("Hello, I am an awesome game !");
-        /*
-        Board board = new Board();
-        Player player1 = new Player("You",  null);
-        Player player2 = new Player("Me", null);
+        GameCreator gameCreator = gameCreatorFromFile("src/main/resources/cards.json");
 
-        Game game = new Game(player1, player2, board);
-        game.startGame();
-        */
+
+    }
+
+    private static GameCreator gameCreatorFromFile(String fileName) {
+        try {
+            BufferedReader buf = new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream(fileName)
+                    )
+            );
+
+            String line = buf.readLine(); StringBuilder sb = new StringBuilder();
+            while(line != null) {
+                sb.append(line).append("\n");
+                line = buf.readLine();
+            }
+            String fileAsString = sb.toString();
+            return new GameCreator(fileAsString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
