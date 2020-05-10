60061b8b89ec12902609b5712ad200b4fd819071
ClarFleu <clarisse.fleu@gmail.com>
2020-05-10 10:11:37 +0200

    Set a few actions for creatures

--- src/main/java/Player/Creature/Creature.java
+++ src/main/java/Player/Creature/Creature.java
@@ -3,15 +3,25 @@ package Player.Creature;
 import ModelClasses.LiveReceptor;
 
 public class Creature extends LiveReceptor {
+    private Spot position;
     private int steps; // exemple
 
-    public Creature(String name, int lifePoints, int steps) {
+    public Creature(String name, int lifePoints, int steps, Spot position) {
         super(name, lifePoints);
         this.steps = steps;
+        this.position = position;
+    }
+
+    public void advance() {
+        // TODO move to position + steps or hit
+    }
+
+    public void retreat() {
+        // TODO move to position - steps
     }
 
     @Override
     public void playTurn(int turn) {
-
+        advance();
     }
 }
