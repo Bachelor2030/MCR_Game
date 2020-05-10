c75c33b32f02361e4ba7bfa621094fe24a712497
ClarFleu <clarisse.fleu@gmail.com>
2020-05-10 09:50:01 +0200

    Change the Card structure

--- src/main/java/Card/Card.java
+++ src/main/java/Card/Card.java
@@ -2,13 +2,16 @@ package Card;
 
 import Command.Macro;
 import ModelClasses.Invocator;
+import Player.Creature.Creature;
 
 public abstract class Card implements Invocator {
     private Macro command;
     private String name;
+    private CardType type;
 
-    public Card(String name) {
-        this.name = name;
+    public Card(String name, CardType type) {
+        this.type = type;
+        this.name = type.toString() + " " + name;
     }
 
     public void play() {
--- src/main/java/Card/TrapCard.java
+++
@@ -1,7 +0,0 @@
-package Card;
-
-public class TrapCard extends Card {
-    public TrapCard(String name) {
-        super("Trap " + name);
-    }
-}
