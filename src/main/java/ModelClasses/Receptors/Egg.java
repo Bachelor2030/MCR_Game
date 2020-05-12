5acfb1051c2fc12757b966bead91c447fd61eeb1
ClarFleu <clarisse.fleu@gmail.com>
2020-05-12 11:43:00 +0200

    Change the structure and add the Egg class

---
+++ src/main/java/ModelClasses/Receptors/Egg.java
@@ -0,0 +1,33 @@
+package ModelClasses.Receptors;
+
+import ModelClasses.LiveReceptor;
+
+public class Egg extends LiveReceptor {
+    private static final int EGG_LIFE_POINTS = 5;
+    private boolean alive;
+
+    public Egg(String name) {
+        super(name, EGG_LIFE_POINTS);
+        alive = true;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    @Override
+    public String toString() {
+        return name + "is " + (alive ? "" : "not ") + "alive" ;
+    }
+
+    @Override
+    public void playTurn(int turn) {}
+
+    @Override
+    public void hit(int points) {
+        super.hit(points);
+        if (lifePoints <= 0) {
+            alive = false;
+        }
+    }
+}
