c3a03d30dbfd6bef8a14685e6f8bae404f1a995f
CosmicElodie <CosmicElodie@users.noreply.github.com>
2020-06-06 18:17:10 +0200

    grosse restructuration : utilisation de java FX, bye bye sprite for da moment!

--- src/main/java/Common/GameBoard/Spot.java
+++ src/main/java/Common/GameBoard/Spot.java
@@ -1,73 +1,79 @@
 package Common.GameBoard;
 
 import Client.Maths.Vector2f;
-import Server.Game.ModelClasses.Receptor;
 import Common.Receptors.Trap;
-import Client.View.Graphics.Sprite;
-import Client.View.Graphics.SpriteSheet;
+import Server.Game.ModelClasses.Receptor;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
-import java.awt.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
-/**
- * Cette classe représente une case qui constitue une ligne de combat
- */
+/** Cette classe représente une case qui constitue une ligne de combat */
 public class Spot {
-    //un case est représentée par un numéro
-    private final int number;
+  // un case est représentée par un numéro
+  private final int number;
 
-    //Compteur de case
-    private static int spotCounter = 0;
+  // Compteur de case
+  private static int spotCounter = 0;
 
-    private Receptor occupant;
+  // l'éventuelle créature présente sur la case
+  private Receptor occupant
+          ;
+  private FileInputStream image = new FileInputStream("src/main/java/Common/GameBoard/island.png");
 
-    protected SpriteSheet sprite;
-    protected Sprite image;
-    protected Vector2f pos;
-    protected int size;
-    protected int spriteX;
-    protected int spriteY;
+  // la position du spot dans l'espace
+  protected Vector2f pos;
 
-    /**
-     * Constructeur de la classe Spot.
-     */
-    public Spot() throws IOException {
-        this(++spotCounter, new Vector2f(), 0);
+  /** Constructeur de la classe Spot. */
+  public Spot() throws IOException {
+    this(++spotCounter, new Vector2f());
+  }
+
+  public Spot(int number, Vector2f pos) throws FileNotFoundException {
+    this.number = number;
+    this.pos = pos;
+  }
+
+  /**
+   * Permet de savoir si une case est occupée par une créature.
+   *
+   * @return true si occupée, false sinon.
+   */
+  public boolean isEmpty() {
+    return occupant == null || occupant.getClass() == Trap.class;
+  }
+
+  /**
+   * Permet de set l'éventuelle créature présente sur la case.
+   *
+   * @param occupant : la créature
+   */
+  public void setOccupant(Receptor occupant) {
+    this.occupant = occupant;
+  }
+
+  /** Modélise le départ d'une créature de la case. */
+  public void leave() {
+    this.occupant = null;
+  }
+
+  /**
+   * Permet de récupérer l'éventuelle créature présente sur la case.
+   *
+   * @return la créature, si elle occupe la case. Sinon null.
+   */
+  public Receptor getOccupant() {
+    return occupant;
+  }
+
+    public FileInputStream getImage() {
+        return image;
     }
 
-    public Spot(SpriteSheet sprite, Vector2f pos, int size) {
-        this(++spotCounter, pos, size);
-        this.sprite = sprite;
-    }
-
-    public Spot(int number, Vector2f pos, int size)
-    {
-        this.number = number;
-        this.pos = pos;
-        this.size = size;
-    }
-
-    /**
-     * Permet de savoir si une case est occupée par une créature.
-     * @return true si occupée, false sinon.
-     */
-    public boolean isEmpty() {
-        return occupant == null || occupant.getClass() == Trap.class;
-    }
-
-    public void setOccupant(Receptor occupant) {
-        this.occupant = occupant;
-    }
-
-    public void leave() {
-        this.occupant = null;
-    }
-
-    public void render(Graphics2D g) {
-        g.drawImage(image.image, (int) (pos.getWorldVar().x), (int) (pos.getWorldVar().y), size, size, null);
-    }
-
-    public Receptor getOccupant() {
-        return occupant;
-    }
 }
--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,45 +1,43 @@
-
 import Client.View.GameBoard;
-
-import java.io.*;
 import Common.GameBoard.Board;
 import Server.Game.ModelClasses.GameClient;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Launcher {
-    private static GameBoard gameBoard; //front-end GUI
-    private static Board board; //back-end LOGIC
+  private static GameBoard gameBoard; // front-end GUI
+  private static Board board; // back-end LOGIC
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("Hello, I am an awesome game !");
-        GameClient gameCreator = gameCreatorFromFile("src/main/resources/cards.json");
+  public static void main(String[] args) throws IOException {
+    System.out.println("Hello, I am an awesome game !");
+    GameClient gameCreator = gameCreatorFromFile("src/main/resources/cards.json");
 
-        initGame();
+    initGame();
+  }
+
+  private static void initGame() throws IOException {
+    board = new Board();
+    gameBoard = new GameBoard();
+  }
+
+  private static GameClient gameCreatorFromFile(String fileName) {
+    try {
+      BufferedReader buf = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+
+      String line = buf.readLine();
+      StringBuilder sb = new StringBuilder();
+      while (line != null) {
+        sb.append(line).append("\n");
+        line = buf.readLine();
+      }
+      String fileAsString = sb.toString();
+      return new GameClient(fileAsString);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-
-    private static void initGame() throws IOException {
-        board = new Board();
-        gameBoard = new GameBoard();
-        gameBoard.start();
-    }
-
-    private static GameClient gameCreatorFromFile(String fileName) {
-        try {
-            BufferedReader buf = new BufferedReader(
-                    new InputStreamReader(
-                            new FileInputStream(fileName)
-                    )
-            );
-
-            String line = buf.readLine(); StringBuilder sb = new StringBuilder();
-            while(line != null) {
-                sb.append(line).append("\n");
-                line = buf.readLine();
-            }
-            String fileAsString = sb.toString();
-            return new GameClient(fileAsString);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+    return null;
+  }
 }
