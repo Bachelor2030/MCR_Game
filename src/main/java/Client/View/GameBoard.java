e5ff28ce72674236fba6ed9ba0c38b956e9d883d
CosmicElodie <CosmicElodie@users.noreply.github.com>
2020-06-13 19:33:22 +0200

    ajout page paramètre. -> doit encore régler width textfield

--- src/main/java/Client/View/GameBoard.java
+++ src/main/java/Client/View/GameBoard.java
@@ -18,6 +18,8 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.*;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.LinkedList;
 
 // TODO : commande qui font des actions graphiques. (genre déplacer créature)
@@ -54,16 +56,23 @@ public class GameBoard extends Application {
 
   private boolean isGaming;
 
-  BorderPane racine;
+  private BorderPane racine;
 
-  Player player1, player2;
-  LinkedList<Card> deck1, deck2;
+  private Player player1, player2;
+  private LinkedList<Card> deck1, deck2;
+  private String
+          namePlayer1, namePlayer2,
+          IpPlayer1, IpPlayer2,
+          portPlayer1, portPlayer2;
+
   private Board board;
 
+  private Stage currentStage;
 
   /** Thread principal du GUI. Gère l'affichage général de la "scene". */
   @Override
   public void start(Stage stage) throws Exception {
+    currentStage = stage;
     //On initialise les players
     player1 = new Player();
     player2 = new Player();
@@ -174,7 +183,7 @@ public class GameBoard extends Application {
     VBox principalMenu = new VBox();
     VBox buttons = new VBox();
 
-    // BACHELOR HUNTERZ
+    // BACHELOR HUNTERZ TITRE
     FileInputStream imagePath = new FileInputStream("src/main/resources/design/images/bh.png");
     ImageView imageMenuView = new ImageView();
     Image imageMenu = new Image(imagePath);
@@ -186,7 +195,7 @@ public class GameBoard extends Application {
     instructionButton.setOnAction(
         actionEvent -> {
           try {
-            printInstructions();
+            displayInstructions();
           } catch (IOException e) {
             e.printStackTrace();
           }
@@ -198,9 +207,8 @@ public class GameBoard extends Application {
     newGameButton.setOnAction(
         actionEvent -> {
           try {
-            isGaming = true;
-            inGame(racine);
-          } catch (IOException e) {
+            displaySettingsMenu();
+          } catch (UnknownHostException e) {
             e.printStackTrace();
           }
         });
@@ -229,11 +237,163 @@ public class GameBoard extends Application {
   }
 
   /**
+   * PAGE DU MENU DE SETTINGS (nom player, adresse IP, port)
+   */
+  private void displaySettingsMenu() throws UnknownHostException {
+    racine.setRight(null);
+    racine.setLeft(null);
+    racine.setTop(null);
+    racine.setBottom(null);
+
+    // ------------------------------------------------------------------
+    // BARRE DE NAVIGATION DE LA PAGE SETTING MENU
+    // ------------------------------------------------------------------
+
+    // On créé une boxe horizontale qui définira l'espace "navigation".
+    HBox barreNavigation = new HBox(10);
+    minimizeButton = new Button();
+    minimizeButton.getStyleClass().add("header-quit-button");
+    quitButton = new Button();
+    quitButton.getStyleClass().add("header-quit-button");
+
+    returnToMenu = new Button("Retourner au menu");
+    returnToMenu.getStyleClass().add("header-button");
+
+    // on règle l'écart du contenu intérieur avec les bords de la boxe
+    barreNavigation.setPadding(new Insets(15, 15, 15, 15));
+    barreNavigation.setPrefWidth(WIDTH_WINDOW);
+
+    // Espace entre les éléments
+    barreNavigation.setSpacing(10);
+
+    // On lui applique d'autres styles présents dans la feuille CSS
+    barreNavigation.getStyleClass().add("header-hbox");
+
+    returnToMenu.setOnAction(
+            actionEvent -> {
+              try {
+                inMainGameMenu();
+              } catch (IOException e) {
+                e.printStackTrace();
+              }
+            });
+    barreNavigation.getChildren().add(returnToMenu);
+
+    // SEPARATEUR - séparer les utility buttons sur la droite
+    // utility buttons : minimize, quit.
+    final Pane spacer = new Pane();
+    HBox.setHgrow(spacer, Priority.ALWAYS);
+    barreNavigation.getChildren().add(spacer);
+
+    // MINIMIZE BUTTON
+    Image minimizeIcon = new Image(getClass().getResourceAsStream("/design/images/minimize.png"));
+    ImageView minimizeIconView = new ImageView(minimizeIcon);
+    minimizeIconView.setFitHeight(21);
+    minimizeIconView.setFitWidth(21);
+    minimizeButton.setGraphic(minimizeIconView); // setting icon to button
+    minimizeButton.setAlignment(Pos.CENTER_RIGHT);
+
+    minimizeButton.setOnAction(
+            event -> {
+              Stage stage = (Stage) minimizeButton.getScene().getWindow();
+              stage.setIconified(true);
+            });
+
+    barreNavigation.getChildren().add(minimizeButton);
+
+    // QUIT BUTTON
+    Image quitIcon = new Image(getClass().getResourceAsStream("/design/images/quit.png"));
+    ImageView quitIconView = new ImageView(quitIcon);
+    quitIconView.setFitHeight(21);
+    quitIconView.setFitWidth(21);
+    quitButton.setGraphic(quitIconView); // setting icon to button
+    quitButton.setAlignment(Pos.CENTER_RIGHT);
+
+    // On créé un event lié à la fermeture du logiciel.
+    quitButton.setOnAction(
+            event -> {
+              ((Stage) quitButton.getScene().getWindow()).close();
+              // sinon on affiche un message d'alerte -> redemande.
+            });
+    barreNavigation.getChildren().add(quitButton);
+
+    // ------------------------------------------------------------------
+    // CORPS DE LA PAGE MENU SETTINGS
+    // ------------------------------------------------------------------
+    VBox bodyParameters = new VBox(10); //contient les éléments de la page et les affiche verticalement.
+    bodyParameters.getStyleClass().add("parameters-body");
+
+    bodyParameters.prefWidthProperty().bind(currentStage.widthProperty().multiply(0.80));
+
+    //Titre : Settings
+    Label settingsTitle = new Label("Settings");
+    settingsTitle.getStyleClass().add("instructions-title");
+
+    //Nom du joueur (Bouffon n°1 si pas changé :D)
+    Label playerName = new Label("Nom");
+    playerName.getStyleClass().add("parameters-label");
+
+    TextField playerNameField = new TextField();
+    playerNameField.setText("Bouffon n°1");
+    playerNameField.getStyleClass().add("parameters-field");
+
+    //Adresse IP du joueur
+    Label playerIP = new Label("Adresse IP");
+    playerIP.getStyleClass().add("parameters-label");
+
+    TextField playerIpField = new TextField();
+    playerIpField.setText(String.valueOf(InetAddress.getLoopbackAddress())); //récupère l'adresse IP
+    playerIpField.getStyleClass().add("parameters-field");
+
+    //Port du joueur
+    Label playerPort = new Label("Port");
+    playerPort.getStyleClass().add("parameters-label");
+
+    TextField playerPortField = new TextField();
+    playerPortField.setText("8080"); //récupère l'adresse IP
+    playerPortField.getStyleClass().add("parameters-field");
+
+    //On crée un bouton qui va permettre de valider les paramètres et créer une nouvelle partie.
+    Button validateParameters = new Button("Valider");
+    validateParameters.getStyleClass().add("header-button");
+    validateParameters.setOnAction(
+            event -> {
+              isGaming = true;
+              try {
+                //On initialise les données
+                namePlayer1 = playerNameField.getText();
+                IpPlayer1 = playerIpField.getText();
+                portPlayer1 = playerPortField.getText();
+
+                //On passe à la fenêtre d'attente d'adversaire
+                //TODO : remplacer par fenêtre de chargement d'adversaire.
+                inGame(racine);
+              } catch (IOException e) {
+                e.printStackTrace();
+              }
+            });
+
+    bodyParameters.getChildren().addAll(settingsTitle,
+            playerName, playerNameField,
+            playerIP, playerIpField,
+            playerPort, playerPortField,
+            validateParameters);
+    bodyParameters.setAlignment(Pos.CENTER);
+    bodyParameters.setSpacing(50); //espace entre les éléments
+
+    // ------------------------------------------------------------------
+    // REGLAGES RACINE
+    // ------------------------------------------------------------------
+    racine.setTop(barreNavigation);
+    racine.setCenter(bodyParameters);
+  }
+
+  /**
    * PAGE INSTRUCTIONS
    *
    * @throws IOException
    */
-  private void printInstructions() throws IOException {
+  private void displayInstructions() throws IOException {
     racine.setRight(null);
     racine.setLeft(null);
     racine.setTop(null);
