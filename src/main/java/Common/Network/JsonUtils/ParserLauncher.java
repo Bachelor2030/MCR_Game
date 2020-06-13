cf621c0378c10cab70676d5ee149c497a5772d9c
CosmicElodie <CosmicElodie@users.noreply.github.com>
2020-06-13 15:29:54 +0200

    règle error

--- src/main/java/Common/Network/JsonUtils/ParserLauncher.java
+++ src/main/java/Common/Network/JsonUtils/ParserLauncher.java
@@ -24,7 +24,7 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class ParserLauncher {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         String file = "src/main/resources/json/game.json";
 
         Game game = parseJsonGame(file);
