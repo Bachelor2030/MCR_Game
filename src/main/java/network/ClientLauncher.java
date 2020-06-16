80b0b1775e9da8e1ddfd10056187b3f4fc028208
ClarFleu <clarisse.fleu@gmail.com>
2020-06-16 13:00:49 +0200

    Remove error

--- src/main/java/network/ClientLauncher.java
+++ src/main/java/network/ClientLauncher.java
@@ -1,8 +1,10 @@
 package network;
 
+import gui.GameBoard;
+
 public class ClientLauncher {
     public static void main(String[] args) {
-        ClientAdapter client = new ClientAdapter(args[0], Integer.valueOf(args[1]), args[2]);
+        ClientAdapter client = new ClientAdapter(new GameBoard(), args[0], Integer.valueOf(args[1]), args[2]);
         client.run();
         System.out.println("Hello, world!");
     }
