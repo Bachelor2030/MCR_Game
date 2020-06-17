e7364ce3ddf96a38c5f6faff5072457d36f03604
CosmicElodie <CosmicElodie@users.noreply.github.com>
2020-06-17 21:57:37 +0200

    dfghjm

--- src/main/java/gui/board/GUIBoard.java
+++ src/main/java/gui/board/GUIBoard.java
@@ -1,10 +1,7 @@
 package gui.board;
 
-import gameLogic.receptors.Player;
-import gameLogic.receptors.Receptor;
 import gui.receptors.GUIPlayer;
 import gui.receptors.GUIReceptor;
-import javafx.scene.Group;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
@@ -17,51 +14,54 @@ import java.util.LinkedList;
  */
 public class GUIBoard {
 
-    //TODO : rentre paramétrique.
-    //le nombre de lignes
-    private final int NB_LINES = 4;
+  // TODO : rentre paramétrique.
+  // le nombre de lignes
+  private final int NB_LINES = 4;
 
-    //compteur de ligne
-    private static int lineCounter;
+  // compteur de ligne
+  private static int lineCounter;
 
-    //les lignes du board
-    private LinkedList<GUILine> GUILines;
+  // les lignes du board
+  private LinkedList<GUILine> GUILines;
 
-    /**
-     * Constructeur de la classe GUIBoard
-     */
-    public GUIBoard(GridPane gridPane, VBox vbox, GUIPlayer player1, GUIPlayer player2) throws IOException {
-        lineCounter = 0;
-        GUILines = new LinkedList<>();
-        for(int line = 0; line < NB_LINES; ++line) {
-            GUILines.add(new GUILine(lineCounter++, gridPane, vbox, player1, player2, player1.getClientSharedState()) );
-        }
+  /** Constructeur de la classe GUIBoard */
+  public GUIBoard(GridPane gridPane, VBox vbox, GUIPlayer player1, GUIPlayer player2)
+      throws IOException {
+    lineCounter = 0;
+    GUILines = new LinkedList<>();
+    for (int line = 0; line < NB_LINES; ++line) {
+      GUILines.add(
+          new GUILine(
+              lineCounter++, gridPane, vbox, player1, player2, player1.getClientSharedState()));
     }
+  }
 
-    public void place(GUIReceptor receptor, int lineCounter, int spot) {
-        GUILines.get(lineCounter).setReceptor(receptor, spot);
-    }
+  public void place(GUIReceptor receptor, int lineCounter, int spot) {
+    GUILines.get(lineCounter).setReceptor(receptor, spot);
+  }
 
-    public GUILine getLine(int index) {
-        if (index < GUILines.size()) {
-            return GUILines.get(index);
-        }
-        return null;
+  public GUILine getLine(int index) {
+    if (index < GUILines.size()) {
+      return GUILines.get(index);
     }
+    return null;
+  }
 
-    /**
-     * Permet de récupérer la liste des lignes du board.
-     * @return la liste des lignes du board.
-     */
-    public LinkedList<GUILine> getGUILines() {
-        return GUILines;
-    }
+  /**
+   * Permet de récupérer la liste des lignes du board.
+   *
+   * @return la liste des lignes du board.
+   */
+  public LinkedList<GUILine> getGUILines() {
+    return GUILines;
+  }
 
-    /**
-     * Permet de savoir le nombre de lignes constituant un board.
-     * @return le nombre de lignes constituant un board.
-     */
-    public int getNB_LINES() {
-        return NB_LINES;
-    }
+  /**
+   * Permet de savoir le nombre de lignes constituant un board.
+   *
+   * @return le nombre de lignes constituant un board.
+   */
+  public int getNB_LINES() {
+    return NB_LINES;
+  }
 }
